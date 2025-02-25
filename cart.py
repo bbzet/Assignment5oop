79a97e4a6c66575e8416710ee36c745af62a54c6
Baiastan Zamirbekov <baastan569@gmail.com>
2025-02-25 16:04:08 +0600

    Updated run application

--- cart.py
+++ cart.py
@@ -14,27 +14,30 @@ class Cart:
         return f'Not enough stock for {amount} {device.name}'
 
     def remove_device(self, device:Device, amount):
-        if device in self.items:
-            if self.items.count(device) >= amount:
-                self.items.remove(device)
+        for i in self.items:
+            if device == i['device']:
+                self.items.remove({'device': device, 'amount': amount})
                 self.total_price -= amount * device.price
                 device.stock += amount
-            else:
-                return f'Not enough {device.name} in cart'
+                return f'{amount} {device.name} removed from cart'
         else:
-            return f'{device.name} not in cart'
+            return f'{device.name} is not in cart'
 
     def get_total_price(self):
         return self.total_price
 
     def print_items(self):
         for device in self.items:
-            print(device.name)
+            print(device['device'].name)
 
     def checkout(self):
-        for device in self.items:
-            self.remove_device(device, self.items.count(device))
-        return f'Your total price is {self.total_price}. Thank you for shopping with us'
+        for i in self.items:
+            result = f'Your total price is {self.total_price}. Thank you for shopping with us'
+            self.remove_device(i['device'], i['amount'])
+            return result
+        else:
+            return 'Your cart is empty'
+
 
 
 
